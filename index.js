9fde8bc7b76470c8e585d7778eb009ff865386b4
William Holmes <holmesw@ie.ibm.com>
2018-03-16 11:50:02 +0000

    Update console logs

--- index.js
+++ index.js
@@ -20,16 +20,19 @@ app.on('message-created', (message, annotation) => {
     const { content = '', spaceId } = message;
     console.log('CONTENT', content);
     _.each(content.match(constants.regex.WEB_SHOT), str => {
-        console.log('str', str);
+        console.log('webshot match', str);
         const url = strings.chompLeft(str.toLowerCase(), constants.regex.KEY);
         console.log('URL', url);
         const filePath = `./${constants.TEMP_DIR}/webshot_${_.now()}.png`;
-        webshot(urlL, filePath, err => {
+        console.log('filePath', filePath);
+        webshot(url, filePath, err => {
+            logger.error('WEBSHOT response', url);
             app.sendMessage(spaceId, data);
             if (_.isEmpty(err)) {
                 app.sendFile(spaceId,filePath);
                 del.sync(filePath, { force: true });
             } else {
+                logger.error('WEBSHOT ERROR', err);
                 app.sendMessage(spaceId, Object.assign({
                     type: 'generic',
                     version: '1',
