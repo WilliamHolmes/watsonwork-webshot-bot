b22810b971066cd023be6ebbe509201cb73c1064
William Holmes <holmesw@ie.ibm.com>
2018-03-16 07:08:20 +0000

    Add logging

--- index.js
+++ index.js
@@ -18,10 +18,13 @@ app.authenticate().then(() => app.uploadPhoto('./appicon.jpeg'));
 
 app.on('message-created', (message, annotation) => {
     const { content = '', spaceId } = message;
+    console.log('CONTENT', content);
     _.each(content.match(constants.regex.WEB_SHOT), str => {
-        const URL = strings.chompLeft(str.toLowerCase(), constants.regex.KEY);
+        console.log('str', str);
+        const url = strings.chompLeft(str.toLowerCase(), constants.regex.KEY);
+        console.log('URL', url);
         const filePath = `./${constants.TEMP_DIR}/webshot_${packageName}.png`;
-        webshot(URL, filePath, err => {
+        webshot(urlL, filePath, err => {
             app.sendMessage(spaceId, data);
             if (_.isEmpty(err)) {
                 app.sendFile(spaceId,filePath);
